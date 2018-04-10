3c4209af7c097529e2fb73acf1df8fba92b33d50
MartinLykke <32263396+MartinLykke@users.noreply.github.com>
2018-04-10 13:59:36 +0200

    javadoc

--- src/BL/Case.java
+++ src/BL/Case.java
@@ -20,43 +20,73 @@ public class Case {
         cCitizen = citizen;
         cJournal = journal;
     }
-
+/**
+ * 
+ * @return 
+ */
     public String getCdate() {
         return cdate;
     }
-
+/**
+ * 
+ * @param cdate 
+ */
     public void setCdate(String cdate) {
         this.cdate = cdate;
     }
-
+/**
+ * 
+ * @return 
+ */
     public String getcStatus() {
         return cStatus;
     }
-
+/**
+ * 
+ * @param cStatus 
+ */
     public void setcStatus(String cStatus) {
         this.cStatus = cStatus;
     }
-
+/**
+ * 
+ * @return 
+ */
     public String getcResponsible() {
         return cResponsible;
     }
-
+/**
+ * 
+ * @param cResponsible 
+ */
     public void setcResponsible(String cResponsible) {
         this.cResponsible = cResponsible;
     }
-
+/**
+ * 
+ * @return 
+ */
     public String getcCitizen() {
         return cCitizen;
     }
-
+/**
+ * 
+ * @param cCitizen 
+ */
     public void setcCitizen(String cCitizen) {
         this.cCitizen = cCitizen;
     }
-
+/**
+ * 
+ * @return 
+ */
     public String getcJournal() {
         return cJournal;
     }
-
+/**
+ * 
+ * @param cJournal 
+ */
     public void setcJournal(String cJournal) {
         this.cJournal = cJournal;
     }
