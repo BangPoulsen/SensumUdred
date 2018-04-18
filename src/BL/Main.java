92ecf638bf02481eda28d46e27ca7bf7a29e3d08
Michael poulsen <fartillucas@gmail.com>
2018-04-18 11:57:52 +0200

    merge

--- src/BL/Main.java
+++ src/BL/Main.java
@@ -1,26 +1,40 @@
 package BL;
 
+        import javafx.application.Application;
+        import javafx.fxml.FXMLLoader;
+        import javafx.scene.Parent;
+        import javafx.scene.Scene;
+        import javafx.stage.Stage;
+
+        import javax.activation.DataHandler;
+        import javax.xml.crypto.Data;
         import java.sql.*;
         import java.util.Scanner;
 
 
-public class Main {
-    public Main() throws SQLException {
-    }
+public class Main extends Application {
+/*    public Main() throws SQLException {
+    }*/
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("GUI.fxml"));
+        primaryStage.setTitle("");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
 
     public static void main(String[] args) {
-        try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-    String url ="jdbc:postresql://stampy.db.elephantsql.com:5432/pjgbvjcy";
-    String username ="pjgbvjcy";
-    String pasword = "eLDL8lqV2NwnApxtHn9DtBQorsPYEwls";
+        launch(args);
 
-    Connection db = DriverManager.getConnection(url, username, pasword);
-    //for testing
-    //Caseworker cw=new Caseworker("ole", "månen", "112", "cirkus@blabla.dk");
+        DatabaseHandler dbHandler = new DatabaseHandler();
+        dbHandler.run ();
+
+    }
 }
+
+
+
+
+
+
