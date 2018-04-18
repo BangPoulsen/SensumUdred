af97211a734f8d481ac32dd4d2cbc791e6ec158a
hrmalte <maltehesk@hotmail.com>
2018-04-18 11:46:27 +0200

    DatabaseHandler added

--- src/BL/GUIController.java
+++ src/BL/GUIController.java
@@ -30,7 +30,7 @@ import javafx.stage.Stage;
  *
  * @author Simon
  */
-public class GUIController extends Application implements Initializable {
+public class GUIController implements Initializable {
 
     @FXML
     private AnchorPane frontPage;
@@ -55,49 +55,23 @@ public class GUIController extends Application implements Initializable {
         // TODO
     }    
     
-    @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("GUI.fxml"));
-        
-        Scene scene = new Scene(root);
-        
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        
-        try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
     
-    String url ="jdbc:postresql://stampy.db.elephantsql.com:5432/pjgbvjcy";
-    String username ="pjgbvjcy";
-    String pasword = "eLDL8lqV2NwnApxtHn9DtBQorsPYEwls";
     
-    try{
-        Connection db = DriverManager.getConnection(url, username, pasword);
-    } catch (SQLException e) {
-        e.printStackTrace();
-    }
-    
-        launch(args);
-        
-        
-    }
     public void Login(ActionEvent event) {
     
-        if (loginUsername.getText().equals("user") && loginPassword.getText().equals("password")) {
-            loginSensumLabel.setText("Logged in as + ");
+        if (loginUsername.getText().equalsIgnoreCase("user") && 
+            loginPassword.getText().equals("password")) {
+            
+            loginSensumLabel.setText("Logged in as " + getNameFromDatabase());
         } else {
             loginSensumLabel.setText("Wrong username or password");
         }
             
             
+    }
+
+    private String getNameFromDatabase() {
+        //Return the name of a person in database
+        return "name";
     }
 }
--- src/BL/Main.java
+++ src/BL/Main.java
@@ -1,16 +1,37 @@
 package BL;
 
-        import java.sql.*;
-        import java.util.Scanner;
+import java.sql.*;
+import java.util.Scanner;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.application.Application;
 
 
-public class Main {
-    public Main() throws SQLException {
+public class Main extends Application{
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("GUI.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
     }
 
-
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
+    
+        DatabaseHandler dbHandler = new DatabaseHandler();
+        dbHandler.run();
+    
+        launch(args);
         
-    //for testing
-    //Caseworker cw=new Caseworker("ole", "månen", "112", "cirkus@blabla.dk");
+    }
+
 }
