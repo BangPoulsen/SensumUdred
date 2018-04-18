326c94eba4fb2668a4307b92f7022668d108fe6c
SteveJ0bs <simonplougmann@gmail.com>
2018-04-18 08:43:36 +0200

    FXID's and controllers

--- src/BL/GUIController.java
+++ src/BL/GUIController.java
@@ -7,14 +7,35 @@ package BL;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+        
 /**
  * FXML Controller class
  *
  * @author Simon
  */
 public class GUIController implements Initializable {
+
+    @FXML
+    private AnchorPane FrontPage;
+    @FXML
+    private AnchorPane LoginPage;
+    @FXML
+    private TextField LoginUsername;
+    @FXML
+    private PasswordField LoginPassword;
+    @FXML
+    private Button LoginButton;
+    @FXML
+    private Button LogoffButton;
+    @FXML
+    private Label LoginSensumLabel;
 
     /**
      * Initializes the controller class.
