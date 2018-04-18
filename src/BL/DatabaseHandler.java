18b89594ec0f634dabad36f57a70163fbbed5123
hrmalte <maltehesk@hotmail.com>
2018-04-18 12:02:18 +0200

    Getname test method

--- src/BL/DatabaseHandler.java
+++ src/BL/DatabaseHandler.java
@@ -7,7 +7,9 @@ package BL;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  *
@@ -15,26 +17,47 @@ import java.sql.SQLException;
  */
 public class DatabaseHandler {
     
+    Connection db;
+    
     public DatabaseHandler(){
         
     }
     
     public void run(){
         String url = "jdbc:postgresql://stampy.db.elephantsql.com:5432/pjgbvjcy";
-    String username ="pjgbvjcy";
-    String pasword = "eLDL8lqV2NwnApxtHn9DtBQorsPYEwls";
+        String username ="pjgbvjcy";
+        String pasword = "eLDL8lqV2NwnApxtHn9DtBQorsPYEwls";
     
-    try {
-        Class.forName("org.postgresql.Driver");
-    } catch (ClassNotFoundException e) {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
-    }
+        }
         
-    try{
-        Connection db = DriverManager.getConnection(url, username, pasword);
-    } catch (SQLException e) {
-        e.printStackTrace();
+        try{
+            db = DriverManager.getConnection(url, username, pasword);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
+    
+    public String getName(String CPR){
+        try {
+
+			Statement st = db.createStatement();
+			ResultSet rs = st.executeQuery("SELECT name FROM users WHERE CPRnummer = 'CPR'");
+
+                            while (rs.next()) {
+                                String name = rs.getString("CPRnummer");
+                                System.out.println("Name gotten: " + name);
+                                
+                                return name;
+                            }
+                            
+			} catch (Exception e) {
+                            System.out.println(e);		
+                        }
+        return "Name not found";
     }
     
 }
--- src/BL/Main.java
+++ src/BL/Main.java
@@ -1,38 +1,5 @@
 package BL;
 
-<<<<<<< HEAD
-        import javafx.application.Application;
-        import javafx.fxml.FXMLLoader;
-        import javafx.scene.Parent;
-        import javafx.scene.Scene;
-        import javafx.stage.Stage;
-
-        import javax.activation.DataHandler;
-        import javax.xml.crypto.Data;
-        import java.sql.*;
-        import java.util.Scanner;
-
-
-public class Main extends Application {
-/*    public Main() throws SQLException {
-    }*/
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("GUI.fxml"));
-        primaryStage.setTitle("");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
-    }
-
-    public static void main(String[] args) {
-        launch(args);
-
-        DatabaseHandler dbHandler = new DatabaseHandler();
-        dbHandler.run ();
-
-    }
-=======
 import java.sql.*;
 import java.util.Scanner;
 import static javafx.application.Application.launch;
@@ -67,7 +34,6 @@ public class Main extends Application{
         
     }
 
->>>>>>> 704cd6fb15992541d778573818e4cf108643b5ea
 }
 
 
