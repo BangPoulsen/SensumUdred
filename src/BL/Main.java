171318787609ea99f728469c3b6eeb5486f61395
BangPoulsen <rasmus@rasmuspoulsen.dk>
2018-04-25 13:34:36 +0200

    Removed useless code

--- src/BL/Main.java
+++ src/BL/Main.java
@@ -15,13 +15,10 @@ public class Main {
      */
     public static void main(String[] args) {
         DatabaseHandler dbHandler = new DatabaseHandler();
-        System.out.println("Opening connection");
         dbHandler.run();
-        System.out.println("Connection opened");
         GUIController guiController = new GUIController();
         guiController.StartApplication();
         dbHandler.closeConnection();
-        System.out.println("Closed connection");
     }
     
 
